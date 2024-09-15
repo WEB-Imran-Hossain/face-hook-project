96bddc67399efa5e61e6ffc839a84d9142e38962
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-09-15 23:03:25 +0600

    page components create

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+import ProfilePage from './pages/ProfilePage';
+import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+     <Routes>
+        <Route element={<HomePage />} path="/" exact />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<ProfilePage />} path="/me" />
+        <Route element={<RegistrationPage />} path="/register" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes>
     </>
   );
 }
