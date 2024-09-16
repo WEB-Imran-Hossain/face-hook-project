e67de4880f451986213b626686f0abef162c30e3
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-09-16 12:45:34 +0600

    Login page working properly

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Field from "../../common/Field";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+    const navigate=useNavigate();
   const {
     register,
     handleSubmit,
@@ -11,13 +13,15 @@ const LoginForm = () => {
 
   const submitForm = (formData) => {
     console.log(formData);
+    navigate("/")
   };
   return (
     <form
       className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
       onSubmit={handleSubmit(submitForm)}
     >
-      <Field label="Email">
+      {/* email field */}
+      <Field label="Email" error={errors.email}>
         <input
           {...register("email", { required: "Email ID is Required" })}
           className={`auth-input ${
@@ -27,6 +31,33 @@ const LoginForm = () => {
           name="email"
           id="email"
         />
+      </Field>
+      {/* password field */}
+      <Field label="Password" error={errors.password}>
+        <input
+          {...register("password", {
+            required: "Password ID is Required",
+            minLength: {
+              value: 8,
+              message: "Your password must be at least 8 characters",
+            },
+          })}
+          className={`auth-input ${
+            !!errors.password ? "border-red-500" : "border-gray-200"
+          }`}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </Field>
+      {/* submit button */}
+      <Field>
+        <button
+          className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+          type="submit"
+        >
+          Login
+        </button>
       </Field>
     </form>
   );
